60ac9effdcfbd44001d0d01fe3199df457cc9828
Jonah Weinbaum <weinbaumjonah@gmail.com>
2023-05-27 17:16:47 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "investor-paradise-41039",
-  url: "https://investor-paradise-41039.botics.co",
-  api: "https://investor-paradise-41039.botics.co/api/v1"
-}
+  "name": "investor-paradise-41039",
+  "url": "https://investor-paradise-41039.botics.co",
+  "api": "https://investor-paradise-41039.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
