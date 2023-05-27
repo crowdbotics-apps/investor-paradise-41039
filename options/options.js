93d50a9ccd64a8e7a1e945cd93ec48f64033a2fb
Crowdbotics <team@crowdbotics.com>
2023-05-27 20:47:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "investor-paradise-41039",
+  url: "https://investor-paradise-41039.botics.co",
+  api: "https://investor-paradise-41039.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
